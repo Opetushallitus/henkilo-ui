afd10223eaef4e3b33f600b914b17ce88aac543d
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-10-24 15:48:49 +0300

    KJHH-1471: Fix flow errors

--- src/main/static/src/validation/EmailValidator.js
+++ src/main/static/src/validation/EmailValidator.js
@@ -2,6 +2,6 @@
 
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 export function validateEmail(email: string): boolean {
-    const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegexp = /\S+@\S+\.\S+/;
     return emailRegexp.test(String(email).toLowerCase());
 }
