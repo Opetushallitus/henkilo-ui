230586e4dd178b4880fd8660d9b577f1b8e168af
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-11-27 15:35:48 +0200

    KJHH-1471: Dev-konfiguraatioon samat poikkeukset kuin normaaliin konfiguraatioon

--- src/main/java/fi/vm/sade/henkiloui/configurations/security/SecurityDevConfiguration.java
+++ src/main/java/fi/vm/sade/henkiloui/configurations/security/SecurityDevConfiguration.java
@@ -46,6 +46,7 @@ public class SecurityDevConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/rekisteroidy").permitAll()
                 .antMatchers("/salasananresetointi/*/*").permitAll()
                 .antMatchers("/sahkopostivarmistus/*/*").permitAll()
+                .antMatchers("/sahkopostivarmistus/virhe/*/*/*").permitAll()
                 // Admin domain
                 .antMatchers("/admin/**").hasAuthority("APP_HENKILONHALLINTA_OPHREKISTERI")
                 .anyRequest().authenticated();
