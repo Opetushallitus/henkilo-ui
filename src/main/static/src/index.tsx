88fe8cc85a4a4782d2ea0844d10683329541948f
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-04-11 15:09:45 +0300

    KJHH-2328 add proper types for dispatch and rootstate (caveat: used any as a type argument for e.g. async actions since typescript can't deduce correct type for some reason)

--- src/main/static/src/index.tsx
+++ src/main/static/src/index.tsx
@@ -5,13 +5,10 @@ import { Provider } from 'react-redux';
 import { Router, useRouterHistory } from 'react-router';
 import { createHistory } from 'history';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
 
 import routes from './routes';
 import PropertySingleton from './globals/PropertySingleton';
-import rootReducer from './reducers';
-import { kayttooikeusApi } from './api/kayttooikeus';
+import { store } from './store';
 
 import './reset.css';
 import './general-style.css';
@@ -20,24 +17,7 @@ import './index.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import './flex.css';
 
-const isDev = process.env.NODE_ENV !== 'production';
-const isClient = typeof window !== 'undefined';
-
 const App = () => {
-    const store = configureStore({
-        reducer: {
-            ...rootReducer,
-            [kayttooikeusApi.reducerPath]: kayttooikeusApi.reducer,
-        },
-        middleware: (getDefaultMiddleware) =>
-            getDefaultMiddleware({
-                thunk: true,
-                immutableCheck: false,
-                serializableCheck: false,
-            }).concat(kayttooikeusApi.middleware),
-        devTools: isDev && isClient,
-    });
-    setupListeners(store.dispatch);
     const browserHistory = useRouterHistory(createHistory)({
         basename: '/henkilo-ui',
     });
